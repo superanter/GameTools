9c1b3594d68ea7c0117980d0725b5ac4da503549
superanter <anter@qq.com>
2018-03-21 13:21:44 +0800

    编辑Excel

--- GameTools/SourceCodes/SoftVersionClass.cs
+++ GameTools/SourceCodes/SoftVersionClass.cs
@@ -31,7 +31,7 @@ namespace AnterStudio.GameTools
         public SoftVersion WiiSaveVersion = new SoftVersion("v1.1.1", "2017-08-02");
         public SoftVersion SwitchSaveVersion = new SoftVersion("v0.0.0", "2017-08-02");
         public SoftVersion DsRomVersion = new SoftVersion("v1.0.0", "2017-08-02");
-        public SoftVersion AmiiboVersion = new SoftVersion("v1.1.0", "2018-01-30");
+        public SoftVersion AmiiboVersion = new SoftVersion("v1.1.0", "2018-03-15");
         public SoftVersion OtherToolsVersion = new SoftVersion("v1.0.0", "2017-08-18");
     }
 }
